32c2f8269e3f941435144a3e6f2a3583267ff4d6
yejikim <erosiezz@gmail.com>
2023-05-08 16:39:12 +0900

    2

---
+++ ruledbased.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import csv
+
+df = pd.read_csv('qna.csv', encoding='cp949')
+df = df.fillna('0')
+df['results'] = df['result1']+df['result2']+df['result3']
+# print(df)
+df.to_csv('qqnnaa.csv', encoding='cp949')
+result_list = []
+choose = []
+
+# for i in range(0, len(df), 4):
+#     sliced_df = df.iloc[i:i+4]
+#     print(sliced_df)
+
+
+def question(q_num):
+    num = q_num*4
+    ques = df[num:num+4]
+    if choose[q_num] == df['answer'][num]:
+        result_list[q_num] = df['reslut']
+    result = result_list[q_num]
