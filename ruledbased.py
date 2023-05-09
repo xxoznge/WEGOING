4a9049ede6389e0bb2b70be824474b7e2308b7bd
yejikim <erosiezz@gmail.com>
2023-05-09 22:07:45 +0900

    일단은...?

--- ruledbased.py
+++ ruledbased.py
@@ -7,24 +7,52 @@ df = pd.read_csv('qna.csv', encoding='cp949')
 df = df.fillna('0')
 df['results'] = df['result1'] + ', ' + df['result2'] + ', ' + df['result3']
 df = df.drop(['result1', 'result2', 'result3'], axis=1)
+
+#답변을 abcd로 바꾸기
+a=[0]*(len(df))
+b=[0]*(len(df))
+c=[0]*(len(df))
+d=[0]*(len(df))
+for i in range(0, len(df),4):
+    a[i]=df['answer'][i]
+    b[i]=df['answer'][i+1]
+    c[i]=df['answer'][i+2]
+    d[i]=df['answer'][i+3]
+
+aa = [x for x in a if x != 0]
+bb = [x for x in b if x != 0]
+cc = [x for x in c if x != 0]
+dd = [x for x in d if x != 0]
+
+df['answer'].replace(aa,'a',inplace=False)
+df['answer'].replace(bb,'b',inplace=False)
+df['answer'].replace(cc,'c',inplace=False)
+df['answer'].replace(dd,'d',inplace=False)
+
 # print(df)
 # df.to_csv('qqnnaa.csv', encoding='cp949')
 
+#사용자가 선택한 내용을 저장한 csv
+ch = pd.read_csv('choose.csv',encoding='cp949')
+#print(ch)
 # 필요한 리스트 생성
 result_list = []
-choose = [] # 가져와야하는 리스트....
 
 def question(q_num): # 규칙기반알고리즘
     num = q_num*4 # 행 인덱스
     ques = df[num:num+4] # 관련 질문 행 4개
 
-    if choose[q_num] == df['answer'][num]:
+    if ch['choose'][q_num] == df['answer'][num]:
         result_list[q_num] = df['resluts']
-    elif choose[q_num] == df['answer'][num+1]:
+    elif ch['choose'][q_num] == df['answer'][num+1]:
         result_list[q_num] = df['resluts']
-    elif choose[q_num] == df['answer'][num+2]:
+    elif ch['choose'][q_num] == df['answer'][num+2]:
         result_list[q_num] = df['resluts']
-    elif choose[q_num] == df['answer'][num+3]:
+    elif ch['choose'][q_num] == df['answer'][num+3]:
         result_list[q_num] = df['resluts']
+    
+    print(ch['choose'][q_num])
+
+hello = question(0)
 
 
