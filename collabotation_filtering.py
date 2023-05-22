2d9337ef8acfea0de3fa87499ff92e0de11fbc35
yejikim <erosiezz@gmail.com>
2023-05-22 15:50:00 +0900

    협업_3

--- collabotation_filtering.py
+++ collabotation_filtering.py
@@ -6,12 +6,25 @@ import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 import operator
 
-## 자기가 좋았던 여행지 입력
+## 자기가 좋았던 여행지 입력 **
 ## 나랑 유사한 사용차 찾기
 ## 리스트 비교 후 없는 내용 출력
 
-## @ 사용자가 입력한 여행지를 코드에서 받아와야하나? 아니면 csv로 받아오나???@##
-
 like = pd.read_csv('ilike.csv', encoding='cp949')
 places = pd.read_csv('places.csv', encoding='cp949')
+# 좋았던 여행지 입력받기
+## 여행지 입력받는 개수를 정해놔야할까?
+for i in range(4):
+    user_id = input("당신의 별명을 알려주세요 ")
+    types = input("당신의 여행 성향은 무엇인가요? ")
+    country = input("좋았던 나라는 어디인가요? ")
+    city = input("어떤 도시를 여행했나요? ")
+
+    like = like.append({'user_id' : user_id, 'type' : types, 'country' : country, 'city' : city})
+
+same_type = pd.DataFrame(columns=['user_id', 'country', 'city'])
+for i in range(len(places)):
+    if places['type'][i] == like['type'][0]:
+        same_type = same_type.append({'user_id' : places['user_id'][i], 'country' : places['liked_country'][i], 'city' : places['liked_city'][i]})
+    
 
